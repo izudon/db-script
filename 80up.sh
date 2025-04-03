f9c776c361309923e14bc011fccfb940f3543861
Masakazu IZUI <i***@g***.com>
2025-04-03 14:49:44 +0900

    add 80up.sh 80down.sh

---
+++ 80up.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# 実効ユーザ root 以外での実行を抑止
+if [ "$(id -un)" != "root" ]; then
+  echo "This script must be run as root user."
+  exit 1
+fi
+
+ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+systemctl reload nginx
